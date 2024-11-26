182ce819da0a6e02a9d43848b14660086fe8f6c7
Alex Kaszynski <akascap@gmail.com>
2024-11-26 13:38:18 -0700

    add support for python 3.13

--- setup.py
+++ setup.py
@@ -95,6 +95,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
     python_requires=">=3.9",
     ext_modules=ext_modules,
